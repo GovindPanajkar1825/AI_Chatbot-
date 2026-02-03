8fba7e214492e504d50939c73fd6dd8a341564d2
Govind Prashant Panajkar <govindapanajkar@gmail.com>
2026-02-03 19:15:00 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, jsonify
+from chatbot_model import get_response
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get", methods=["POST"])
+def chat():
+    user_input = request.form["message"]
+    response = get_response(user_input)
+    return jsonify({"response": response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
+  
